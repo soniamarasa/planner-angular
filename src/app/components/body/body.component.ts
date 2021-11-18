2beb09b37160efb59b1ea58877dd1e0daeed9077
soniamarasa <soniamarasa@hotmail.com>
2021-11-18 18:30:16 -0300

    pin

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ItemsFacade } from 'src/app/facades/items.facade';
 import { Item } from 'src/app/models/item';
+import { DateService } from 'src/app/services/date.service';
 import { ItemsService } from 'src/app/services/items.service';
 
 @Component({
@@ -10,15 +11,16 @@ import { ItemsService } from 'src/app/services/items.service';
   styleUrls: ['./body.component.scss'],
 })
 export class BodyComponent implements OnInit {
+  weekDay: any
 
   public items$: Observable<Item[]> | undefined;
 
-  constructor(public facade: ItemsFacade) {
+  constructor(public facade: ItemsFacade, public date: DateService) {
   }
 
   ngOnInit(): any {
-    // this.items$ = this.
-    this.facade.getAllItems();
+    this.weekDay = this.date.weekDay.toLowerCase();
+    // this.facade.getAllItems();
   }
   
 
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { ItemsFacade } from 'src/app/facades/items.facade';
+import { DateService } from 'src/app/services/date.service';
 
 @Component({
   selector: 'app-card',
@@ -10,13 +11,19 @@ import { ItemsFacade } from 'src/app/facades/items.facade';
 export class CardComponent implements OnInit {
   items$ = this.facade.itemsState$.pipe(map((state) => state.items));
 
+  weekDay: any;
+
   @Input() titleBox: string | undefined;
   @Input() day: string | undefined;
   @Input() box: string | undefined;
   value: Date | undefined;
 
-  constructor(public facade: ItemsFacade) {}
+  constructor(public facade: ItemsFacade, public date: DateService) {}
 
   ngOnInit(): void {
+    this.day = this.day?.toLowerCase();
+
+    this.weekDay = this.date.wD.toLowerCase();
+    console.log(this.day, this.weekDay);
   }
 }
